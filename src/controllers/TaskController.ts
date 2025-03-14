eb42138851dc81966cb2dbd595a90a1650c3755b
HiramDev <hiramdev02@gmail.com>
2025-03-13 20:36:36 -0600

    feature: endpoint to update state was created

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -96,4 +96,22 @@ export class TaskController {
       res.status(500).json({ error: "Hubo un error" });
     }
   };
+
+  static updateStatus = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { taskId } = req.params;
+      const task = await Task.findById(taskId);
+      if (!task) {
+        const error = new Error("Proyecto no encontrado");
+        res.status(404).json({ error: error.message });
+        return;
+      }
+      const { status } = req.body;
+      task.status = status;
+      await task.save();
+      res.send("Tarea actualizada");
+    } catch (error) {
+      res.status(500).json({ error: "Hubo un error" });
+    }
+  };
 }
