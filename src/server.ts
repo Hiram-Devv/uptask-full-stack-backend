6f1d3596cff137f3305e5535178275ced65db6f5
HiramDev <hiramdev02@gmail.com>
2025-04-26 11:49:14 -0600

    morgan was installed

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import morgan from "morgan";
 import { corsConfig } from "./config/cors";
 import { connectDb } from "./config/db";
 import projectRoutes from "./routes/projectRoutes";
@@ -11,6 +12,10 @@ connectDb();
 const app = express();
 app.use(cors(corsConfig));
 
+// Logging
+app.use(morgan("dev"));
+
+// Leer datos de formularios
 app.use(express.json());
 // Routes
 app.use("/api/projects", projectRoutes);
